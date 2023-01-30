c1da387a9b0ab396854e4842d616bb36000bdd84
Caroline Robinson <CRobinsonAtWork@gmail.com>
2023-01-30 12:53:41 -0900

    pulled github issues creation package, renamed package, then changed github repo name in this interview to that package

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distutils.util import convert_path
 
 standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
 standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
 def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
     out = {}
     stack = [(convert_path(where), '', package)]
@@ -45,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.Plurals',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.Plurals\n\nA docassemble extension.\n\n## Author\n\nCaroline Robinson, CRobinson@akcourts.gov\n\n',
+      long_description='# docassemble.Plurals\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nCaroline Robinson, CRobinson@akcourts.gov\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Caroline Robinson',
       author_email='CRobinson@akcourts.gov',
